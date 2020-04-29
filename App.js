15f42bfe52ce261d2b222d9fabd53cd9fb9c353d
mauri-m <maury__25@hotmail.com>
2020-04-29 16:38:11 -0300

    v1

---
+++ App.js
@@ -0,0 +1,54 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Fragment} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+         <MapView
+           provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+           style={styles.map}
+           region={{
+             latitude: 37.78825,
+             longitude: -122.4324,
+             latitudeDelta: 0.015,
+             longitudeDelta: 0.0121,
+           }}
+         >
+         </MapView>
+       </View>
+  );
+};
+
+const styles = StyleSheet.create({
+ container: {
+   ...StyleSheet.absoluteFillObject,
+   height: 400,
+   width: 400,
+   justifyContent: 'flex-end',
+   alignItems: 'center',
+ },
+ map: {
+   ...StyleSheet.absoluteFillObject,
+ },
+});
+
+
+
+export default App;
